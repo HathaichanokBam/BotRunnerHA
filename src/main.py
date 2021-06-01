99b329c0be0b2f72f8bcbd5fec24d447cffa0a78
HathaichanokBam <h.nakarapanich@gmail.com>
2021-06-01 09:38:54 +0700

    function for move queued task & modified move schedule

--- src/main.py
+++ src/main.py
@@ -1,30 +1,64 @@
 # import asyncio
-from callAPI import checkDevice, checkUnattendedBot, deleteSchedule, getSchedule, getToken, runSchedule
+from callAPI import checkDevice, checkUnattendedBot, deleteSchedule, deploymentStatus, getSchedule, getToken, runSchedule
 from datetime import datetime, timedelta
 from time import sleep
 
 url = 'http://10.226.107.101/'
 tasks_unsuccess = []
 devices = {}
+unattendedIDs = {}          # {"ID": "assignTasks_count"}
+availableIDs = {}           # {"ID": "assignTasks_count"}
 fileIDs = []
 timeSchedule = []
-
+tasksInQueue = []
+runningTasks = []
+isAvailable = True
 
 while (True):
     token = getToken(url)
     # print(token)
+    tasksInQueue, runningTasks = deploymentStatus(url, token)
     '''
     tasks_unsuccess = auditLog(url, token)
     for task in tasks_unsuccess:
         print(task)
     '''
+    # check device status
     devices = checkDevice(url, token)
     for deviceID in devices["CONNECTED"]:
         unattendedID = checkUnattendedBot(url, token, deviceID)
         if unattendedID != "No Unattended Bot":
-            break
+            for eachRunningTask in runningTasks:
+                print(eachRunningTask["userId"])
+                if eachRunningTask["userId"] == unattendedID:      # connected & unattended but running task
+                    isAvailable = False
+                    unattendedIDs[unattendedID] = 0
+            if isAvailable:
+                availableIDs[unattendedID] = 0          # connected & unattended & available
+    print(availableIDs)
+    print(unattendedIDs)
 
-    if unattendedID == "No Unattended Bot":
+    # move queued tasks to available bot
+    minAssignTask = 100
+    assignID = 0
+    for eachTask in tasksInQueue:
+        # print(eachTask)
+        # print(availableIDs)
+        if availableIDs != {}:
+            for availableID in availableIDs:            # find ID that has minimum assign tasks
+                if availableIDs[availableID] < minAssignTask:
+                    minAssignTask = availableIDs[availableID]
+                    assignID = availableID
+
+            now = datetime.now()
+            date_time = now + timedelta(minutes=1)
+            date, time = str(date_time).split(" ")
+            runSchedule(url, token, eachTask["fileId"], eachTask["fileName"], assignID, date, time[0:5])
+            availableIDs[assignID] += 1
+            deleteSchedule(url, token, eachTask['id'])
+
+    # move schedule for disconnected devices
+    if unattendedIDs == {}:
         print("No available bot")
     else:
         if devices["DISCONNECTED"] == []:
@@ -42,14 +76,39 @@ while (True):
                             date_time = datetime.strptime(date + " " + time, '%Y-%m-%d %H:%M:%S')
                             now = datetime.now()
 
-                            if date_time > now:
-                                runSchedule(url, token, i["fileID"], i["fileName"], unattendedID, j['startDate'], j['startTime'])
-                            else:   # it's possible??
-                                date_time = date_time + timedelta(minutes=10)
-                                date, time = str(date_time).split(" ")
-                                runSchedule(url, token, i["fileID"], i["fileName"], unattendedID, date, time[0:5])
+                            if (availableIDs != {}):
+                                minAssignTask = 100
+                                assignID = 0
+                                for availableID in availableIDs:
+                                    if availableIDs[availableID] < minAssignTask:
+                                        minAssignTask = availableIDs[availableID]
+                                        assignID = availableID
+
+                                if date_time > now:
+                                    runSchedule(url, token, i["fileID"], i["fileName"], assignID, j['startDate'], j['startTime'])
+                                else:   # it's possible??
+                                    date_time = date_time + timedelta(minutes=10)
+                                    date, time = str(date_time).split(" ")
+                                    runSchedule(url, token, i["fileID"], i["fileName"], assignID, date, time[0:5])
+                                availableIDs[assignID] += 1
+                            else:
+                                minAssignTask = 100
+                                assignID = 0
+                                for unattendedID in unattendedIDs:
+                                    if unattendedIDs[unattendedID] < minAssignTask:
+                                        minAssignTask = unattendedIDs[unattendedID]
+                                        assignID = unattendedID
+
+                                if date_time > now:
+                                    runSchedule(url, token, i["fileID"], i["fileName"], assignID, j['startDate'], j['startTime'])
+                                else:   # it's possible??
+                                    date_time = date_time + timedelta(minutes=10)
+                                    date, time = str(date_time).split(" ")
+                                    runSchedule(url, token, i["fileID"], i["fileName"], assignID, date, time[0:5])
+                                unattendedIDs[assignID] += 1
 
                             deleteSchedule(url, token, i['id'])
             else:
                 print("No failed tasks")
+
     sleep(60)
